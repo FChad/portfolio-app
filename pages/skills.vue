983f4505581a5d09d77b0eaff8e80dc4fac8448a
FChad <feiersteinchad@gmail.com>
2024-03-21 19:20:34 +0100

    skills finished

--- pages/skills.vue
+++ pages/skills.vue
@@ -1,7 +1,7 @@
 <template>
     <PageHeader :title="$t('skills.pageHeader')" />
 
-    <div class="max-w-[600px] my-4">
+    <div class="max-w-[600px] my-8">
         <h3>{{ $t('skills.sectionCompetencies.title') }}</h3>
         <p>{{ $t('skills.sectionCompetencies.description') }}</p>
     </div>
@@ -11,13 +11,13 @@
         <div>
             <span
                 class="flex items-center justify-center gap-2 text-xl font-bold bg-blue-500 text-white rounded-lg text-center p-2 my-2 shadow-lg">
-                <Icon name="i-material-symbols-school" class="w-6 h-6" /> Bildung
+                <Icon name="i-material-symbols-school" class="w-6 h-6" /> {{ $t('skills.education.title') }}
             </span>
             <ul>
                 <li v-for="item in $tm('skills.education.items')" class="card">
                     <span class="flex items-center justify-start gap-1">
                         <Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5" />{{
-                    extractValue(item.year) }}
+        extractValue(item.year) }}
                     </span>
 
                     <NuxtLink v-if="item.link" :to="extractValue(item.link)" target="_blank">
@@ -38,7 +38,7 @@
         <div>
             <span
                 class="flex items-center justify-center gap-2 text-xl font-bold bg-blue-500 text-white rounded-lg text-center p-2 my-2 shadow-lg">
-                <Icon name="i-material-symbols-language" class="w-6 h-6" /> Sprachen
+                <Icon name="i-material-symbols-language" class="w-6 h-6" /> {{ $t('skills.languages.title') }}
             </span>
             <ul>
                 <li v-for="item in $tm('skills.languages.items')" class="card">
@@ -56,14 +56,15 @@
         <div>
             <span
                 class="flex items-center justify-center gap-2 text-xl font-bold bg-blue-500 text-white rounded-lg text-center p-2 my-2 shadow-lg">
-                <Icon name="i-material-symbols-verified-rounded" class="w-6 h-6" /> Zertifizierungen
+                <Icon name="i-material-symbols-verified-rounded" class="w-6 h-6" /> {{ $t('skills.certifications.title')
+                }}
             </span>
             <ul>
 
                 <li v-for="item in $tm('skills.certifications.items')" class="card">
                     <span class="flex items-center justify-start gap-1">
                         <Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5" />{{
-                    extractValue(item.year) }}
+        extractValue(item.year) }}
                     </span>
 
                     <NuxtLink v-if="item.link" :to="extractValue(item.link)" target="_blank">
@@ -80,7 +81,7 @@
         </div>
     </div>
 
-    <div class="max-w-[600px] my-4">
+    <div class="max-w-[600px] my-8">
         <h3>{{ $t('skills.sectionWork.title') }}</h3>
         <p>{{ $t('skills.sectionWork.description') }}</p>
     </div>
@@ -99,7 +100,7 @@
                 <div class="w-full flex justify-between">
                     <span v-if="item.date" class="flex items-center justify-start gap-1">
                         <Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5" /> {{
-                    extractValue(item.date) }}
+        extractValue(item.date) }}
                     </span>
                     <span v-if="item.duration">{{ extractValue(item.duration) }}</span>
                 </div>
@@ -108,10 +109,10 @@
             <p v-if="item.description">{{ extractValue(item.description) }}</p>
 
             <UButton v-if="item.tasks" icon='i-heroicons-window-20-solid' color="gray" variant="solid" class="p-2 my-2"
-                @click="openModal(index)">
+                @click="openModal('job' + index)">
                 {{ $t('skills.work.show_more') }}</UButton>
 
-            <UModal v-if="item.tasks" :model-value="openModalIndex === index"
+            <UModal v-if="item.tasks" :model-value="openModalIndex === 'job' + index"
                 @update:model-value="value => value ? openModal(index) : closeModal()">
                 <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
                     <template #header>
@@ -120,17 +121,21 @@
                             <div class="w-full flex justify-between">
 
                                 <span v-if="item.company" class="font-medium flex items-center justify-start gap-1">
-                                    <Icon name="i-material-symbols-work" class="w-5 h-5" /> {{ $t('skills.work.company') }}
+                                    <Icon name="i-material-symbols-work" class="w-5 h-5" /> {{ $t('skills.work.company')
+                                    }}
                                     {{ extractValue(item.company) }}
                                 </span>
-                                <span v-if="item.type" class="font-medium">{{ $t('skills.work.type') }} {{ extractValue(item.type) }}</span>
+                                <span v-if="item.type" class="font-medium">{{ $t('skills.work.type') }} {{
+        extractValue(item.type) }}</span>
                             </div>
                             <div class="w-full flex justify-between">
                                 <span v-if="item.date" class="flex items-center justify-start gap-1">
-                                    <Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5" />{{ $t('skills.work.date') }} {{
-                    extractValue(item.date) }}
+                                    <Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5" />{{
+        $t('skills.work.date') }} {{
+        extractValue(item.date) }}
                                 </span>
-                                <span v-if="item.duration">{{ $t('skills.work.duration') }} {{ extractValue(item.duration) }}</span>
+                                <span v-if="item.duration">{{ $t('skills.work.duration') }} {{
+        extractValue(item.duration) }}</span>
                             </div>
                         </div>
                     </template>
@@ -146,9 +151,46 @@
         </div>
     </div>
 
-    <div class="max-w-[600px] my-4">
+    <div class="max-w-[600px] my-8">
         <h3>{{ $t('skills.sectionOtherSkills.title') }}</h3>
         <p>{{ $t('skills.sectionOtherSkills.description') }}</p>
+    </div>
+
+    <div class="grid grid-cols-[repeat(auto-fill,_minmax(300px,_1fr))] w-full gap-8 m-auto">
+        <div class="card" v-for="(item, index) in $tm('skills.otherSkills.items')">
+            <div class="leading-[30px] flex justify-between items-center flex-wrap relative">
+                <h4 class="flex items-center justify-start gap-1 text-xl font-bold w-full">
+                    <Icon :name="extractValue(item.icon)" class="w-6 h-6" />{{ extractValue(item.title) }}
+                </h4>
+            </div>
+            <div class="w-full h-[2px] bg-gray-300 mt-2 mb-[14px]"></div>
+            <p>{{ extractValue(item.description) }}</p>
+
+            <UButton v-if="item.details" icon='i-heroicons-window-20-solid' color="gray" variant="solid"
+                class="p-2 my-2" @click="openModal('skill' + index)">
+                {{ $t('skills.otherSkills.show_more') }}</UButton>
+
+            <UModal v-if="item.details" :model-value="openModalIndex === 'skill' + index"
+                @update:model-value="value => value ? openModal(index) : closeModal()">
+                <UCard :ui="{ ring: '', divide: 'divide-y divide-gray-100 dark:divide-gray-800' }">
+                    <template #header>
+                        <div class="flex justify-between items-center flex-wrap relative">
+                            <h4 class="flex items-center justify-start gap-1 text-xl font-bold w-full">
+                                <Icon :name="extractValue(item.icon)" class="w-6 h-6" />{{ extractValue(item.title) }}
+                            </h4>
+                            <p><span class="font-medium">{{ $t('skills.otherSkills.description') }}</span> {{ extractValue(item.description) }}</p>
+                        </div>
+                    </template>
+
+                    <template v-for="categoryItems in item.details">
+                        <p class="text-lg font-medium my-2">{{ extractValue(categoryItems.category) }}</p>
+                        <ul class="list-disc pl-6">
+                            <li v-for="detailItems in categoryItems.items"><span class="font-medium">{{ extractValue(detailItems.type) }}:</span> {{ extractValue(detailItems.description) }}</li>
+                        </ul>
+                    </template>
+                </UCard>
+            </UModal>
+        </div>
     </div>
 </template>
 
