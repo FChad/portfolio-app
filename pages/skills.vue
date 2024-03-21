677384ae3ebf047c6841dfac4caea6b8f6032225
FChad <feiersteinchad@gmail.com>
2024-03-21 13:17:33 +0100

    added function extractValue(obj)

--- pages/skills.vue
+++ pages/skills.vue
@@ -14,17 +14,17 @@
             </span>
             <ul>
                 <li v-for="item in $tm('skills.education.items')" :key="item.year" class="card">
-                    <span class="flex items-center justify-start gap-1"><Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5"/>{{ item.year.body.static }}</span>
+                    <span class="flex items-center justify-start gap-1"><Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5"/>{{ extractValue(item.year) }}</span>
 
-                    <NuxtLink v-if="item.link" :to="item.link.body.static" target="_blank">
+                    <NuxtLink v-if="item.link" :to="extractValue(item.link)" target="_blank">
                         <Icon name="i-material-symbols-open-in-new-rounded" class="w-5 h-5 absolute top-0 right-0 m-2 cursor-pointer text-blue-500"/>
                     </NuxtLink>
 
                     <p>
-                        <span class="font-medium" v-if="item.degree">{{ item.degree.body.static }}</span>
-                        <span v-if="item.field"><br>{{ item.field.body.static }}</span>
-                        <span v-if="item.school"><br>{{ item.school.body.static }}</span>
-                        <span v-if="item.grade"><br>{{ item.grade.body.static }}</span>
+                        <span class="font-medium" v-if="item.degree">{{ extractValue(item.degree) }}</span>
+                        <span v-if="item.field"><br>{{ extractValue(item.field) }}</span>
+                        <span v-if="item.school"><br>{{ extractValue(item.school) }}</span>
+                        <span v-if="item.grade"><br>{{ extractValue(item.grade) }}</span>
                     </p>
                 </li>
             </ul>
@@ -37,9 +37,9 @@
             <ul>
                 <li v-for="item in $tm('skills.languages.items')" :key="item.language" class="card">
                     <div class="flex justify-between items-center mb-1">
-                        <img :src="item.flag.body.static" alt="Luxemburgische Flagge" class="max-w-[30px] rounded-[6px]">
-                        <span v-if="item.language">{{ item.language.body.static }}</span>
-                        <span v-if="item.level">{{ item.level.body.static }}</span>
+                        <img :src="extractValue(item.flag)" alt="Luxemburgische Flagge" class="max-w-[30px] rounded-[6px]">
+                        <span v-if="item.language">{{ extractValue(item.language) }}</span>
+                        <span v-if="item.level">{{ extractValue(item.level) }}</span>
                     </div>
                     <UProgress v-if="item.percentage" :value="item.percentage" />
                 </li>
@@ -53,15 +53,15 @@
             <ul class="edu-list">
 
                 <li v-for="item in $tm('skills.certifications.items')" :key="item.certification" class="card">
-                    <span class="flex items-center justify-start gap-1"><Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5"/>{{ item.year.body.static }}</span>
+                    <span class="flex items-center justify-start gap-1"><Icon name="i-material-symbols-calendar-month-rounded" class="w-5 h-5"/>{{ extractValue(item.year) }}</span>
 
-                    <NuxtLink v-if="item.link" :to="item.link.body.static" target="_blank">
+                    <NuxtLink v-if="item.link" :to="extractValue(item.link)" target="_blank">
                         <Icon name="i-material-symbols-open-in-new-rounded" class="w-5 h-5 absolute top-0 right-0 m-2 cursor-pointer text-blue-500"/>
                     </NuxtLink>
 
                     <p>
-                        <span v-if="item.title" class="font-medium">{{ item.title.body.static }}</span>
-                        <span v-if="item.details"><br>{{ item.details.body.static }}</span>
+                        <span v-if="item.title" class="font-medium">{{ extractValue(item.title) }}</span>
+                        <span v-if="item.details"><br>{{ extractValue(item.details) }}</span>
                     </p>
                 </li>
             </ul>
@@ -74,6 +74,17 @@
     </div>
 </template>
 
-<script setup></script>
+<script setup>
+
+function extractValue(obj) {
+    if (obj.hasOwnProperty('body')) {
+        return obj['body']['static'];
+    } else if (obj.hasOwnProperty('b')) {
+        return obj['b']['s'];
+    }
+  return null;
+}
+
+</script>
 
 <style scoped></style>
